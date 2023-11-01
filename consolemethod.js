1eb87affb7a1c07a63325dc43ba72a8e65044a85
Asif Ekhlaque <132199879+Asifekhlaque@users.noreply.github.com>
2023-11-01 20:01:32 +0530

    Add files via upload

---
+++ consolemethod.js
@@ -0,0 +1,17 @@
+// console method is a way to perform operations on a string
+console.log("Hello World!");
+console.assert(1==1);
+console.clear();
+console.count();
+obj={
+    name:"Asif",
+    age:19
+}
+console.table(obj)
+console.time("This is time")
+console.timeEnd("This is time")
+console.info("This is info")
+console.warn("This is warn")
+console.error("This is error")
+console.trace("This is trace")
+console.dir(obj,true)
